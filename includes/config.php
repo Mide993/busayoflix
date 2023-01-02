98f019e21577f587eff10aff3ae775ac872d38b8
Mide993 <117308499+Mide993@users.noreply.github.com>
2023-01-02 09:39:35 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -5,7 +5,7 @@ session_start();
 date_default_timezone_set("Europe/London");
 
 try {
-    $con  = new PDO("mysql:dbname=busayoflix;host=localhost", "root", "");
+    $con  = new PDO("mysql:dbname=busayoflix;host=busayoflix1.mysql.database.azure.com", "busayo@busayoflix1", "Password12");
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }
 catch (PDOException $e) {
